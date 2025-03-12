a13efc1c455627190e2ac6264de62896772f4766
martin bamber <mbamber1986@gmail.com>
2025-03-12 00:16:35 +0000

    Changed Way Interface handled Methods to support chainloading

--- src/Interface/WriterInterface.php
+++ src/Interface/WriterInterface.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace FireCore\DataHandler\Interface;
+namespace LazarusPhp\FileHandler\Interface;
 
 interface WriterInterface
 {
     public function decodeData();
 
     // Adds a new keypair
-    public function set(string $key,string|int $value):void;
+    public function set(string $key,string|int $value);
     // Removes a specific Key pair
-    public function remove(string $key,string|int $value):void;
+    public function remove(string $key);
     // Destroys entire section
     public function destroy():void;
     
