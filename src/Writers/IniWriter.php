3e81fada4958a36de56cb7be93b1354dc26aba53
martin bamber <mbamber1986@gmail.com>
2025-03-12 00:14:41 +0000

    Creatd New IniWriter

---
+++ src/Writers/IniWriter.php
@@ -0,0 +1,60 @@
+<?php
+namespace LazarusPhp\FileHandler\Writers;
+
+use LazarusPhp\FileHandler\CoreFiles\WriterCore;
+use LazarusPhp\FileHandler\Interface\WriterInterface;
+
+class IniWriter extends WriterCore  implements WriterInterface
+{
+
+    private static $hasSections;
+
+
+    public function parseFile($name,$hasSections=true)
+    {
+        self::$name = $name;
+        self::$hasSections = $hasSections;
+        $file = self::$path[$name];
+        self::$data = parse_ini_file($file,self::$hasSections);
+    }
+
+    public function get($section,$key)
+    {
+        return self::$data[$section][$key];
+    }
+
+    public function decodeData()
+    {
+        
+    }
+
+    public function save(?string $name = null)
+    {
+    $name = $name ?? self::$name;
+    $file = self::$path[$name];
+    if(self::detectExtention($file,"ini"))
+    {
+        $content = "";
+        if (self::$hasSections) {
+            foreach (self::$data as $section => $value) {
+                $content .= "[$section]\n";
+                foreach ($value as $key => $val) {
+                    $content .= "$key = " . (is_numeric($val) ? $val : '"' . addslashes($val) . '"') . "\n";
+                }
+            }
+        } else {
+            foreach (self::$data as $key => $val) {
+                $content .= "{$key}={$val} \n";
+            }
+        }
+        file_put_contents($file, $content);
+
+    }
+    else
+    {
+        trigger_error("Failed to save file : ".self::$path[$name]."Incompatible file");
+    }
+    }
+
+
+}
