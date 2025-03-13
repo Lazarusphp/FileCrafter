f92df42cadcf19d6cd5af2edc5af6190c7015aaa
martin bamber <mbamber1986@gmail.com>
2025-03-13 22:42:43 +0000

    Removed dataDecode method

--- src/Interface/WriterInterface.php
+++ src/Interface/WriterInterface.php
@@ -5,7 +5,6 @@ namespace LazarusPhp\FileHandler\Interface;
 interface WriterInterface
 {
     public function __construct(string $name);
-    public function decodeData();
 
     // Adds a new keypair
     public function set(string $key,string|int $value);
